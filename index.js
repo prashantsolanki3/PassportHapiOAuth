7d10cc9b853ad2fd80ab02fe843cb1a73c4130c3
Soheil Hajimohammadi <soheilhajimohammadi@Soheils-MacBook-Pro.local>
2017-10-26 16:50:04 -0700

    updated to es5

--- index.js
+++ index.js
@@ -1,72 +1,102 @@
-import passport from 'passport';
-import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
-import { Strategy as OutlookStrategy } from 'passport-windowslive';
-import { Strategy as FacebookStrategy } from 'passport-facebook';
+'use strict';
 
-export default class HapiPassport {
-    
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _passport = require('passport');
+
+var _passport2 = _interopRequireDefault(_passport);
+
+var _passportGoogleOauth = require('passport-google-oauth');
+
+var _passportWindowslive = require('passport-windowslive');
+
+var _passportFacebook = require('passport-facebook');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var HapiPassport = function () {
+
     /**
      * Constructor
      * @param strategy
      * @param config
      */
-    constructor(strategy, config) {
+    function HapiPassport(strategy, config) {
+        _classCallCheck(this, HapiPassport);
+
         if (typeof strategy !== 'string' || !['GoogleStrategy', 'OutlookStrategy'].includes(strategy)) {
             throw new Error('This module requires a valid strategy upon instantiation, please use one of the following: \'GoogleStrategy\' or \'OutlookStrategy\'');
         }
-        if (typeof config !== 'object') {
+        if ((typeof config === 'undefined' ? 'undefined' : _typeof(config)) !== 'object') {
             throw new Error('This module requires a valid configuration upon instantiation');
         }
         if (strategy === 'GoogleStrategy') {
             this._provider = 'google';
-            passport.use(new GoogleStrategy(config, (accessToken, refreshToken, profile, done) => {
-                done(null, profile.emails[0].value); 
+            _passport2.default.use(new _passportGoogleOauth.OAuth2Strategy(config, function (accessToken, refreshToken, profile, done) {
+                done(null, profile.emails[0].value);
             }));
         } else if (strategy === 'OutlookStrategy') {
             this._provider = 'windowslive';
-            passport.use(new OutlookStrategy(config, (accessToken, refreshToken, profile, done) => {
-                done(null, profile.emails[0].value); 
+            _passport2.default.use(new _passportWindowslive.Strategy(config, function (accessToken, refreshToken, profile, done) {
+                done(null, profile.emails[0].value);
             }));
         } else if (strategy === 'FacebookStrategy') {
             this._provider = 'windowslive';
-            passport.use(new FacebookStrategy(config, (accessToken, refreshToken, profile, done) => {
-                done(null, profile.emails[0].value); 
+            _passport2.default.use(new _passportFacebook.Strategy(config, function (accessToken, refreshToken, profile, done) {
+                done(null, profile.emails[0].value);
             }));
         }
-        passport.serializeUser(function(user, cb) {
+        _passport2.default.serializeUser(function (user, cb) {
             cb(null, user);
-        });        
-        passport.deserializeUser(function(obj, cb) {
+        });
+        _passport2.default.deserializeUser(function (obj, cb) {
             cb(null, obj);
-        });        
-        this._strategy = strategy;       
+        });
+        this._strategy = strategy;
     }
 
-    authenticate(req, callback) {
-        const res = {
-            setHeader(data, url) {
-                callback(url);
+    _createClass(HapiPassport, [{
+        key: 'authenticate',
+        value: function authenticate(req, callback) {
+            var res = {
+                setHeader: function setHeader(data, url) {
+                    callback(url);
+                }
+            };
+            if (this._strategy === 'GoogleStrategy') {
+                _passport2.default.authenticate('google', { scope: ['profile', 'email'] }).call(this, req, res, function () {});
+            } else if (this._strategy === 'OutlookStrategy') {
+                _passport2.default.authenticate('windowslive', { scope: ['wl.signin', 'wl.basic', 'wl.emails'] }).call(this, req, res, function () {});
+            } else if (this._strategy === 'FacebookStrategy') {
+                _passport2.default.authenticate('facebook').call(this, req, res, function () {});
             }
-        };
-        if (this._strategy === 'GoogleStrategy') {
-            passport.authenticate('google', { scope: ['profile', 'email'] }).call(this, req, res, () => {});
-        } else if (this._strategy === 'OutlookStrategy')  {
-            passport.authenticate('windowslive', { scope: ['wl.signin', 'wl.basic', 'wl.emails'] }).call(this, req, res, () => {});   
-        } else if (this._strategy === 'FacebookStrategy')  {
-            passport.authenticate('facebook').call(this, req, res, () => {});   
         }
-    }
-    
-    authenticateCallBack(req, callback) {
-        const request = {
-            query: req.query,
-            logIn: email => callback(null, email)            
-        };
-        passport.authenticate(this._provider, {}).call(this, request, {},  (err) => {
-            if (err) {
-                callback('Failed to authenticate user.');
-            }
-        });        
-    }
+    }, {
+        key: 'authenticateCallBack',
+        value: function authenticateCallBack(req, callback) {
+            var request = {
+                query: req.query,
+                logIn: function logIn(email) {
+                    return callback(null, email);
+                }
+            };
+            _passport2.default.authenticate(this._provider, {}).call(this, request, {}, function (err) {
+                if (err) {
+                    callback('Failed to authenticate user.');
+                }
+            });
+        }
+    }]);
 
-}
+    return HapiPassport;
+}();
+
+exports.default = HapiPassport;
